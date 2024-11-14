ea10e299bd563d634b74576a832e7259b7b723b2
Tyuiu.NazarovaKKH@outlook.com <Tyuiu.NazarovaKKH@outlook.com>
2024-11-14 12:02:36 +0300

    Task2

---
+++ Tyuiu.NazarovaKH.Sprint2.Task2.V2.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.NazarovaKH.Sprint2.Task2.V2.Lib;
+
+namespace Tyuiu.NazarovaKH.Sprint2.Task2.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 3;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NazarovaKH.Sprint2.Task2.V2/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.NazarovaKH.Sprint2.Task2.V2.Lib;
+
+namespace Tyuiu.NazarovaKH.Sprint2.Task2.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной х:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
