6f04dddc0fcfac403322dd3efd3fe45cb9cbf7ef
Tyuiu.NazarovaKKH@outlook.com <Tyuiu.NazarovaKKH@outlook.com>
2024-11-14 18:34:47 +0300

    Task4

--- Tyuiu.NazarovaKH.Sprint2.Task4.V18.Lib/DataService.cs
+++ Tyuiu.NazarovaKH.Sprint2.Task4.V18.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.NazarovaKH.Sprint2.Task4.V18.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = Math.Pow((6 + (x - 1) / Math.Pow(y, 3)), x);
+            double z = x * 3 < y - 2 ? Math.Pow((6 + (x - 1) / Math.Pow(y, 3)), x) : x + 10 * y - (1 / x);
             return Math.Round(z, 3);
         }
     }
